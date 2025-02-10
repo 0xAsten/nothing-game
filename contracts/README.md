884b24203050e2a162c3f5d21956e18f87fbc44a
Asten <0xasten@gmail.com>
2025-02-10 11:06:30 +0800

    deploy to spolia

--- contracts/README.md
+++ contracts/README.md
@@ -1,68 +1,18 @@
-![Dojo Starter](./assets/cover.png)
+## Deploy on sepolia
 
-<picture>
-  <source media="(prefers-color-scheme: dark)" srcset=".github/mark-dark.svg">
-  <img alt="Dojo logo" align="right" width="120" src=".github/mark-light.svg">
-</picture>
+export STARKNET_RPC_URL=https://api.cartridge.gg/x/starknet/sepolia
+export DOJO_ACCOUNT_ADDRESS=
+export DOJO_PRIVATE_KEY=
 
-<a href="https://twitter.com/dojostarknet">
-<img src="https://img.shields.io/twitter/follow/dojostarknet?style=social"/>
-</a>
-<a href="https://github.com/dojoengine/dojo">
-<img src="https://img.shields.io/github/stars/dojoengine/dojo?style=social"/>
-</a>
+sozo build -P release
 
-[![discord](https://img.shields.io/badge/join-dojo-green?logo=discord&logoColor=white)](https://discord.gg/PwDa2mKhR4)
-[![Telegram Chat][tg-badge]][tg-url]
+sozo migrate -P release
 
-[tg-badge]: https://img.shields.io/endpoint?color=neon&logo=telegram&label=chat&style=flat-square&url=https%3A%2F%2Ftg.sumanjay.workers.dev%2Fdojoengine
-[tg-url]: https://t.me/dojoengine
+slot deployments create nothing-game torii \
+ --rpc https://api.cartridge.gg/x/starknet/sepolia \
+ --world 0x0456e4818b95a2fffc646f691e8df897952d2bb7e520232bdeeb26d91259ece9 \
+ --version 1.0.9
 
-# Dojo Starter: Official Guide
+https://api.cartridge.gg/x/nothing-game/torii
 
-A quickstart guide to help you build and deploy your first Dojo provable game.
-
-Read the full tutorial [here](https://dojoengine.org/tutorial/dojo-starter).
-
-## Running Locally
-
-#### Terminal one (Make sure this is running)
-
-```bash
-# Run Katana
-katana --dev --dev.no-fee
-```
-
-#### Terminal two
-
-```bash
-# Build the example
-sozo build
-
-# Inspect the world
-sozo inspect
-
-# Migrate the example
-sozo migrate
-
-# Start Torii
-# Replace <WORLD_ADDRESS> with the address of the deployed world from the previous step
-torii --world <WORLD_ADDRESS> --http.cors_origins "*"
-```
-
----
-
-## Contribution
-
-1. **Report a Bug**
-
-    - If you think you have encountered a bug, and we should know about it, feel free to report it [here](https://github.com/dojoengine/dojo-starter/issues) and we will take care of it.
-
-2. **Request a Feature**
-
-    - You can also request for a feature [here](https://github.com/dojoengine/dojo-starter/issues), and if it's viable, it will be picked for development.
-
-3. **Create a Pull Request**
-    - It can't get better then this, your pull request will be appreciated by the community.
-
-Happy coding!
+./scripts/add_item.sh
--- contracts/scripts/add_item.sh
+++ contracts/scripts/add_item.sh
@@ -49,28 +49,30 @@ echo "--------------------------------------------------------------------------
 # Large Backpack 0x4c61726765204261636b7061636b
 # Magic Satchel 0x4d61676963205361746368656c
 
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 1,0x52757374792053776f7264,1,1,1,2,10,5,0,0,0,0,1 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 2,0x426174746c6520417865,1,1,2,2,20,8,0,0,0,0,1 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 3,0x5761722048616d6d6572,1,2,2,2,40,15,0,0,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 4,0x486f6c7920426c616465,1,3,2,3,100,30,0,0,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 5,0x53686f727420426f77,1,1,1,2,15,6,0,0,0,0,2 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 6,0x5477696e2044616767657273,1,2,1,2,35,12,0,0,0,0,5 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 7,0x4372797374616c205374616666,1,3,1,3,80,25,0,0,0,0,3 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 8,0x4c6561746865722041726d6f72,2,1,2,2,15,0,5,10,0,0,2 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 9,0x436861696e204d61696c,2,1,2,2,25,0,8,15,0,0,1 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 10,0x537465656c20506c617465,2,2,2,2,50,0,15,20,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 11,0x486f6c792041726d6f72,2,3,2,2,120,0,25,30,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 12,0x536861646f772056657374,2,1,2,2,20,0,6,12,0,0,5 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 13,0x526f6775652047617262,2,2,2,2,45,0,12,18,0,0,5 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 14,0x4d61676520526f6265,2,3,2,2,90,0,20,25,0,0,3 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 15,0x57617272696f722052696e67,3,1,1,1,10,0,0,0,1,5,1 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 16,0x4b6e69676874205365616c,3,2,1,1,40,0,0,0,2,10,4 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 17,0x486f6c7920456d626c656d,3,3,1,1,75,0,0,0,2,20,4 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 18,0x48756e746572204d61726b,3,1,1,1,12,0,0,0,1,5,2 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 19,0x536861646f77204d61726b,3,2,1,1,35,0,0,0,1,12,5 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 20,0x4d616765204372797374616c,3,3,1,1,75,15,0,0,1,18,3 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 21,0x536d616c6c20506f756368,4,1,2,2,20,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 22,0x416476656e747572657220426167,4,1,2,3,35,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 23,0x4c61726765204261636b7061636b,4,2,3,3,60,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 24,0x4d61676963205361746368656c,4,3,3,4,100,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
+sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 1,0x4c6561746865722041726d6f72,2,1,2,2,15,0,5,10,0,0,2 --wait --rpc-url $STARKNET_RPC_URL
+sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 2,0x52757374792053776f7264,1,1,1,2,10,5,0,0,0,0,1 --wait --rpc-url $STARKNET_RPC_URL
+sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 3,0x536d616c6c20506f756368,4,1,2,2,20,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
+sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 4,0x5477696e2044616767657273,1,2,1,2,35,12,0,0,0,0,5 --wait --rpc-url $STARKNET_RPC_URL
+sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 5,0x57617272696f722052696e67,3,1,1,1,10,0,0,0,1,5,1 --wait --rpc-url $STARKNET_RPC_URL
+
+
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 6,0x426174746c6520417865,1,1,2,2,20,8,0,0,0,0,1 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 7,0x5761722048616d6d6572,1,2,2,2,40,15,0,0,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 8,0x486f6c7920426c616465,1,3,2,3,100,30,0,0,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 9,0x53686f727420426f77,1,1,1,2,15,6,0,0,0,0,2 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 10,0x4372797374616c205374616666,1,3,1,3,80,25,0,0,0,0,3 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 11,0x436861696e204d61696c,2,1,2,2,25,0,8,15,0,0,1 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 12,0x537465656c20506c617465,2,2,2,2,50,0,15,20,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 13,0x486f6c792041726d6f72,2,3,2,2,120,0,25,30,0,0,4 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 14,0x536861646f772056657374,2,1,2,2,20,0,6,12,0,0,5 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 15,0x526f6775652047617262,2,2,2,2,45,0,12,18,0,0,5 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 16,0x4d61676520526f6265,2,3,2,2,90,0,20,25,0,0,3 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 17,0x4b6e69676874205365616c,3,2,1,1,40,0,0,0,2,10,4 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 18,0x486f6c7920456d626c656d,3,3,1,1,75,0,0,0,2,20,4 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 19,0x48756e746572204d61726b,3,1,1,1,12,0,0,0,1,5,2 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 20,0x536861646f77204d61726b,3,2,1,1,35,0,0,0,1,12,5 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 21,0x4d616765204372797374616c,3,3,1,1,75,15,0,0,1,18,3 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 22,0x416476656e747572657220426167,4,1,2,3,35,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 23,0x4c61726765204261636b7061636b,4,2,3,3,60,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
+# sozo execute --world $WORLD_ADDRESS $ITEM_STSTEM_ADDRESS add_item -c 24,0x4d61676963205361746368656c,4,3,3,4,100,0,0,0,0,0,0 --wait --rpc-url $STARKNET_RPC_URL
 
